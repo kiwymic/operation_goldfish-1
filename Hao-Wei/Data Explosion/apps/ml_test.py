827ae6261b408dd0485c1ae2b15acc3be41d3f02
NYCDSA <nycdsa@NYCDSAs-MacBook-Pro-3.local>
2021-09-04 17:20:23 -0400

    ML test v2. Now support CatBoost and Linear regression

--- Hao-Wei/Data Explosion/apps/ml_test.py
+++ Hao-Wei/Data Explosion/apps/ml_test.py
@@ -7,6 +7,7 @@ import pandas as pd;
 import numpy as np;
 import plotly.express as px;
 import plotly.graph_objs as go;
+import pickle;
 
 from dictionaries import *;
 
@@ -16,18 +17,59 @@ from app import server
 ## Machine learning toolkits
 from catboost import CatBoostRegressor;
 from sklearn.preprocessing import LabelEncoder;
+from sklearn.linear_model import LinearRegression;
 
 # Utility functions which sends from front end to back end
-def front_to_back_cat(fe):
+def front_to_back(fe, method = "cat"):
+    '''
+    Description:
+    This is the function which construct the backend data (which directly feeds to CatBoostRegressor)
+    given the frontend data.
+    Input:
+    fe: The frontend dataframe. Columns must be the same as those in version 6 of the housing data.
+    method: The string which describes the regressor. Default = cat.
+    Compatible values: "cat": CatBoostRegressor; "lm": Multilinear method (with lasso penalization)
+    Output: The backend dataframe. Should be ready to "regressor.fit()".
+    '''
     be = fe.copy();
 
-    be['ExterQualDisc']=be['ExterQual']-be['OverallQual'];
-    be['OverallCondDisc']=be['OverallCond']-be['OverallQual'];
-    be['KitchenQualDisc']=be['KitchenQual']-be['OverallQual'];
-    be=be.drop(["SalePrice", 'ExterQual','OverallCond','KitchenQual'],axis=1);
+    if method == "cat":
+        be['ExterQualDisc']=be['ExterQual']-be['OverallQual'];
+        be['OverallCondDisc']=be['OverallCond']-be['OverallQual'];
+        be['KitchenQualDisc']=be['KitchenQual']-be['OverallQual'];
+        be=be.drop(["SalePrice", 'ExterQual','OverallCond','KitchenQual'],axis=1);
 
-    be = dummify(be, non_dummies, dummies);
+        be = dummify(be, non_dummies, dummies);
+    elif method == "lm":
+        be.drop(columns = ['SalePrice'], axis =1, inplace = True);
+        be['GrLivArea_log'] = np.log10(be['GrLivArea']);
+        be['LotArea_log'] = np.log10(be['LotArea']);
+        be['ExterQualDisc'] = be['ExterQual'] - be['OverallQual'];
+        be['OverallCondDisc'] = be['OverallCond'] - be['OverallQual'];
+        be['KitchenQualDisc'] = be['KitchenQual'] - be['OverallQual'];
+        be = be.drop(['ExterQual','OverallCond','KitchenQual'], axis=1);
+
+        be['BSMT_LowQual_bin'] = pd.cut(be['BSMT_LowQual'], [-1, 1, 500, 1000, 1500, 2500], labels = ['No basement', '0-500', '500-1000', '1000-1500', '1500+']);
+        be['BSMT_HighQual_bin'] = pd.cut(be['BSMT_HighQual'], [-1, 1, 500, 1000, 1500, 2500], labels = ['No basement', '0-500', '500-1000', '1000-1500', '1500+']);
+        be.drop(['BSMT_HighQual', 'BSMT_LowQual', 'GrLivArea', 'LotArea'], axis = 1, inplace = True);
+
+        be = dummify(be, non_dummies_linear, dummies_linear);
     return be;
+
+def predict_from_front(fe, method = "cat"):
+    '''
+    Description:
+    Given a frontend data frame, and a string describing a regressor, predicts.
+    Input:
+    fe: The frontend dataframe. Columns must be the same as those in version 6 of the housing data.
+    method: The string which describes the regressor. Default = "cat". Compatible values: See above.
+    Output: A pd.Series predicting the output.
+    '''
+    if method_dict[method]["Scale"] == "lin":
+        return method_dict[method]["Regressor"].predict(front_to_back(fe, method));
+    else: # The method is log scaled. Needs an exponentiation
+        return 10 ** method_dict[method]["Regressor"].predict(front_to_back(fe, method));
+    return;
 
 housing_address = pd.read_csv('./data/house_coordinates_address.csv', index_col = 0);
 housing = pd.read_csv('./data/ames_housing_price_data_v6.csv', index_col = 0);
@@ -37,12 +79,25 @@ y = front_end["SalePrice"];
 # Loading Mo's frontend to backend code.
 #transformation of front-end to back-end, and catboost application
 
-back_end = front_to_back_cat(front_end);
+back_end = front_to_back(front_end, "lm");
 
+# The zoo of regressors...
 cat = CatBoostRegressor();
-cat.load_model("./data/HousePriceCatBoost", "cbm")
-print(cat.score(back_end, y))
-cat_pred = cat.predict(back_end)
+cat.load_model("./data/HousePriceCatBoost", "cbm");
+
+with open('./data/linearmodel.pickle', mode = 'rb') as file:
+    lm = pickle.load(file);
+
+method_dict = {
+    "cat": {"Scale": "lin", "Order": False, "Regressor": cat,
+            "Altered": ['Neighborhood', 'BldgType', 'MasVnrType', 'ExterQual','OverallCond','KitchenQual']},
+    "lm": {"Scale": "log", "Order": True, "Regressor": lm,
+            "Altered": ['Neighborhood', 'BldgType', 'MasVnrType', 'BSMT_HighQual', 'BSMT_LowQual',
+            'ExterQual','OverallCond','KitchenQual', 'GrLivArea', 'LotArea']}
+};
+##########################
+
+print(lm.score(back_end, np.log10(y)));
 
 # These are the columns to be displayed in a standard DataTable.
 display_columns = {"Sale Price Price":"SalePrice", "Ground Living Area":"GrLivArea", "Quality (1-8)": "Quality",\
@@ -72,7 +127,7 @@ layout = html.Div([
             labelStyle={'display': 'inline-block'},
             className='text-center'
             ),
-            dcc.Graph(id="pid_scatter", clickData=None),
+            dcc.Graph(id="pid_scatter", clickData=None, style={"height": "60%"}),
             dt.DataTable(id='pid_table',
                          columns=[{"name": k, "id": display_columns[k]} for k in display_columns],
                          page_current=0, page_size=PAGE_SIZE, page_action='custom')
@@ -83,6 +138,17 @@ layout = html.Div([
             html.H5("Here is your dream home...", id = "selected_pid",
                     className='text-center text-primary'),
             html.Hr(),
+            html.P("The nitty gritty:"),
+            dcc.Checklist(id = "check_methods",
+            options=[
+                {'label': 'CatBoost', 'value': 'check_cat'},
+                {'label': 'Linear', 'value': 'check_lm'},
+                {'label': 'Linear SVR', 'value': 'check_svr'}],
+            value=['check_cat', 'check_lm'],
+            labelStyle={'display': 'inline-block'},
+            className='text-center'),
+            html.Hr(),
+
             html.P("Let's get ready to flip the house and get filthy rich!"),
 
             html.Div([
@@ -184,73 +250,86 @@ def update_selected_on_click(clickData):
     return "Your selection: " + address;
 #     return clickData['points'][0]['hovertext'];
 
-def flipping_chart(PID, major, minor=None):
+def flipping_chart(PID, major, minor=None, estimator = ["cat"]):
     '''
+    Description:
+    The guy who draws. The core of this file. If you dare deleting this...
+    I will cry.
     Input:
     PID: A nine-digit-ish thing for each property used by USPS or so...
     major: The major variable. Will be the x-axis of a flipping chart.
     minor: The minor variable. If assigned, will summon spaghettis for each unique value.
+    estimator: A list of strings, each represents a regressor.
     Output:
     A flipping chart. Yeah.
     '''
-    temp_house = front_end.reset_index();
-    temp_house = temp_house[temp_house["PID"] == PID];
+    fig = px.scatter();
 
-    # Compute the predicted value of the selected house.
-    maj_val = float(temp_house[major]);
-    temp_frame = temp_house.set_index("PID");
-    pred_price = cat.predict(front_to_back_cat(temp_frame))[0];
+    for method in estimator:
+        temp_house = front_end.reset_index();
+        temp_house = temp_house[temp_house["PID"] == PID];
 
-    temp_house.drop(major, axis = 1, inplace = True);
-    if minor: temp_house.drop(minor, axis = 1, inplace = True);
+        # Compute the predicted value of the selected house.
+        maj_val = float(temp_house[major]);
+        temp_frame = temp_house.set_index("PID");
+        pred_price = predict_from_front(temp_frame, method)[0];
+        # pred_price = cat.predict(front_to_back(temp_frame, "cat"))[0];
 
-    if "Range" in column_title_dict[major]:
-        major_range = np.linspace(column_title_dict[major]["Range"][0],\
-        column_title_dict[major]["Range"][1],\
-        int((column_title_dict[major]["Range"][1]-column_title_dict[major]["Range"][0])/\
-        column_title_dict[major]["Range"][2])+1);
-    else:
-        r_max = front_end[major].max();
-        r_min = front_end[major].min();
-        major_range = np.linspace(r_min, r_max, 300);
-    if minor:
-        minor_range = sorted(list(front_end[minor].unique()));
-        print(minor_range)
+        temp_house.drop(major, axis = 1, inplace = True);
+        if minor: temp_house.drop(minor, axis = 1, inplace = True);
 
-    temp = pd.DataFrame({"PID": [PID for i in major_range], major: major_range});
-    temp_house = temp_house.merge(temp, on="PID");
-    if minor:
-        temp = pd.DataFrame({"PID": [PID for i in minor_range], minor: minor_range});
+        if "Range" in column_title_dict[major]:
+            major_range = np.linspace(column_title_dict[major]["Range"][0],\
+            column_title_dict[major]["Range"][1],\
+            int((column_title_dict[major]["Range"][1]-column_title_dict[major]["Range"][0])/\
+            column_title_dict[major]["Range"][2])+1);
+        else:
+            r_max = front_end[major].max();
+            r_min = front_end[major].min();
+            major_range = np.linspace(r_min, r_max, 300);
+        if minor:
+            minor_range = sorted(list(front_end[minor].unique()));
+
+        temp = pd.DataFrame({"PID": [PID for i in major_range], major: major_range});
         temp_house = temp_house.merge(temp, on="PID");
-        temp_minor = temp_house[minor];
+        temp_major = temp_house[major];
 
-    temp_house.set_index("PID", inplace=True);
-    temp_house = front_to_back_cat(temp_house);
+        if minor:
+            temp = pd.DataFrame({"PID": [PID for i in minor_range], minor: minor_range});
+            temp_house = temp_house.merge(temp, on="PID");
+            temp_minor = temp_house[minor];
 
-    # If the column is killed when moving to backend, pad it.
-    temp_house["Sale Price Predicted"] = cat.predict(temp_house);
-    if minor in ['Neighborhood', 'BldgType', 'MasVnrType', 'ExterQual','OverallCond','KitchenQual']:
-        temp_house.reset_index(inplace = True);
-        temp_house[minor] = temp_minor;
-        print(temp_house[["Sale Price Predicted", minor]])
+        temp_house.set_index("PID", inplace=True);
+        # temp_house = front_to_back(temp_house, "cat");
 
-    if not minor:
-        fig = px.line(temp_house, x=major, y="Sale Price Predicted",\
-        labels={major: column_title_dict[major]["Description"]});
-    else:
-        fig = px.scatter();
-        for i in minor_range:
-            temp = temp_house[temp_house[minor]==i];
+        # If the order of variables matter for the regressor, rearrange it.
+        if method_dict[method]["Order"]:
+            temp_house = temp_house[front_end.columns];
 
-            fig.add_trace(go.Scatter(x=temp[major], y=temp["Sale Price Predicted"], name = str(i)));
+        temp_house["Sale Price Predicted"] = predict_from_front(temp_house, method);
 
-    fig.add_trace(go.Scatter(x=[maj_val], y=[pred_price], name="Current",\
-    marker=dict(size=12, color = '#FF8833')));
+        if not minor:
+            fig.add_trace(go.Scatter(x=temp_house[major], y=temp_house["Sale Price Predicted"],\
+            name = major+'_'+method));
+            # fig = px.line(temp_house, x=major, y="Sale Price Predicted",\
+            # labels={major: column_title_dict[major]["Description"]});
+        else:
+            #fig = px.scatter();
+            for i in minor_range:
+                temp = temp_house[temp_house[minor]==i];
 
-    if not minor:
-        fig.update_layout(showlegend=False);
+                if type(i) in [np.float64, np.int64]: name = "{:.2f}_{}".format(i, method);
+                else: name = i + '_' + method;
+                fig.add_trace(go.Scatter(x=temp[major], y=temp["Sale Price Predicted"], name = name));
 
-    fig.update_layout(legend=dict(yanchor="top", y=0.99, xanchor="left", x=0.01, bgcolor='rgba(0,0,0,0)'));
+        fig.add_trace(go.Scatter(x=[maj_val], y=[pred_price], name="Current_"+method,\
+        marker=dict(size=12)));
+
+        if not minor:
+            fig.update_layout(showlegend=False);
+
+    fig.update_layout(legend=dict(yanchor="top", y=0.99, xanchor="left", x=0.01,\
+    font=dict(size=9), bgcolor='rgba(0,0,0,0)'));
     fig.update_xaxes(title_text=column_title_dict[major]["Description"]);
     fig.update_yaxes(title_text="Sales Price Predicted");
     return fig;
@@ -260,23 +339,28 @@ def flipping_chart(PID, major, minor=None):
     Output('pid_quality_change', 'figure'),
     Input('pid_scatter', 'clickData'),
     Input('flip_major', 'value'),
-    Input('flip_minor', 'value'))
-def update_flip_chart(clickData, major, minor):
+    Input('flip_minor', 'value'),
+    Input('check_methods', 'value'))
+def update_flip_chart(clickData, major, minor, methods):
     if not clickData or not major: return px.scatter();
 
+    methods = [x[6:] for x in methods];
+    print(methods)
     PID = clickData['points'][0]['hovertext'];
-    return flipping_chart(PID, major, minor);
+    return flipping_chart(PID, major, minor, methods);
 
 @app.callback(
     Output('pid_area_change', 'figure'),
     Input('pid_scatter', 'clickData'),
     Input('explore_major', 'value'),
-    Input('explore_minor', 'value'))
-def update_explore_chart(clickData, major, minor):
+    Input('explore_minor', 'value'),
+    Input('check_methods', 'value'))
+def update_explore_chart(clickData, major, minor, methods):
     if not clickData or not major: return px.scatter();
 
+    methods = [x[6:] for x in methods];
     PID = clickData['points'][0]['hovertext'];
-    return flipping_chart(PID, major, minor);
+    return flipping_chart(PID, major, minor, methods);
 
 
 # @app.callback(
